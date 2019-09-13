b35bd5f20a10a618119670576e6cfbb9062ba939
David Kahara <kaharadave@gmail.com>
2019-09-13 10:16:35 +0300

    Update Readme

--- api/user/schema.py
+++ api/user/schema.py
@@ -3,6 +3,7 @@ from graphene_sqlalchemy import SQLAlchemyObjectType
 from graphql import GraphQLError
 from flask_graphql_auth import (create_access_token,
                                 create_refresh_token)
+from flask_bcrypt import check_password_hash
 
 from api.user.models import User as UserModel
 from utils.validators import verify_email, validate_empty_fields
@@ -18,9 +19,8 @@ class User(SQLAlchemyObjectType):
 
 
 class CreateUser(graphene.Mutation):
-    """
-    Mutation that creates a user
-    """
+    """Mutation that creates a user."""
+
     class Arguments:
         name = graphene.String(required=True)
         email = graphene.String(required=True)
@@ -46,8 +46,34 @@ class CreateUser(graphene.Mutation):
         user.save()
 
         return CreateUser(user=user, access_token=create_access_token(
-                          kwargs['name']),
-                          refresh_token=create_refresh_token(kwargs['name']))
+                          kwargs['email']),
+                          refresh_token=create_refresh_token(kwargs['email']))
+
+
+class LoginUser(graphene.Mutation):
+
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+    access_token = graphene.String()
+    refresh_token = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        validate_empty_fields(**kwargs)
+        query = User.get_query(info)
+        email = kwargs.get('email')
+        password = kwargs.get('password')
+        exact_user = query.filter(UserModel.email == email).first()
+        if not verify_email(email):
+            raise GraphQLError('Invalid email format')
+        if not exact_user:
+            raise GraphQLError('This email does not exist')
+        if not check_password_hash(exact_user.password_hash, password):
+            raise GraphQLError('Please check your login credentials')
+
+        return LoginUser(access_token=create_access_token(
+                        kwargs['email']),
+                          refresh_token=create_refresh_token(kwargs['email']))
 
 
 class Mutation(graphene.ObjectType):
@@ -59,6 +85,11 @@ class Mutation(graphene.ObjectType):
             \n- email: The email field of the user[required]\
             \n- name: The name field of a user[required]\
             \n- password_hash: The password field of a user[required]")
+
+    login_user = LoginUser.Field(
+        description="Logs in a user with the arguments\
+            \n- email: The email field of the user[required]\
+            \n- password: The password field of a user[required]")
 
 
 class Query(graphene.ObjectType):
--- utils/utility.py
+++ utils/utility.py
@@ -2,11 +2,7 @@ import enum
 from helpers.database import db_session
 
 
-<<<<<<< HEAD
-class Utility(object):
-=======
 class ModelOperations(object):
->>>>>>> 2274939ae6978efa5dbb73c0d35d66e41bfb60d9
     def save(self):
         """Function that saves new objects"""
         db_session.add(self)
